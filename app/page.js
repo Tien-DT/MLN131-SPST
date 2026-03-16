f4a23746de95eeda4c9fca276bbc5d630faee25c
Ngoc Thach <ngocthachtruongnguyen@gmail.com>
2026-03-16 21:45:15 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -18,7 +18,7 @@ export default function Home() {
         <div className="container-custom">
           <h2 className="font-serif-heading text-2xl mb-4 text-[#F4D03F]">Kỷ nguyên Độc lập (1975 - 1981)</h2>
           <p className="font-serif-body text-[#D1C2A5] max-w-xl mx-auto mb-8">
-            Học phần: Chủ nghĩa Xã hội Khoa học <br/>
+            Học phần: Lịch sử Đảng Cộng Sản Việt Nam<br/>
             Đề tài: Đảng lãnh đạo cả nước xây dựng chủ nghĩa xã hội và bảo vệ Tổ quốc
           </p>
           <div className="w-24 h-1 bg-[#D1C2A5] mx-auto opacity-30"></div>
